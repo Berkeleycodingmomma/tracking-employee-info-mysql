74e1d4e2a9835457be63e71d28cee3211818847f
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-30 17:45:25 -0600

    added the switch cases

--- server.js
+++ server.js
@@ -274,3 +274,77 @@ const viewEmpByMgr = async () => {
         })
 }
 
+//Below I have added the prompt questions
+
+const chooseRequest = () => {
+    inquirer.prompt([{
+            type: 'list',
+            name: 'request',
+            message: 'What would you like to do?',
+            choices: ['Add a Department',
+                'Add an Employee',
+                'Add a Role',
+                'Delete and Employee',
+                'Update Employees Role',
+                'Update Employees Manager',
+                'View All Departments',
+                'View All Employess',
+                'View All Roles',
+                'View Department Budget',
+                'View Employees by Department',
+                'View Employees by Manager',
+            ],
+            loop: false
+        }, ])
+        .then((data) => {
+            const {
+                request
+            } = data;
+            console.log(request);
+
+            // Switch Case
+            switch (request) {
+                case 'Add a Department':
+                    newDept();
+                    break;
+                case 'Add a Role':
+                    newRole();
+                    break;
+                case 'Add an Employee':
+                    newEmp();
+                    break;
+                case 'Delete an Employee':
+                    delEmp();
+                    break;
+                case 'Update Employees Role':
+                    updateEmpRole();
+                    break;
+                case 'Update Employees Manager':
+                    updateEmpManager();
+                    break;
+                case 'View all Departments':
+                    viewDepts();
+                    break;
+                case 'View all Employees':
+                    newEmp();
+                    break;
+                case 'View all Roles':
+                    viewRoles();
+                    break;
+                case 'VIew Department Budget':
+                    viewBudgets();
+                    break;
+                case 'View Employees by Department':
+                    viewEmpByIdDept();
+                    break;
+                case 'View Employees by Manager':
+                    viewEmpByMgr();
+                    break;
+
+                default:
+                    break;
+            }
+        })
+}
+
+chooseRequest();
