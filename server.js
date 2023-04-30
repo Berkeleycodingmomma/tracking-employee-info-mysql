6c2e69cc2a546f30682f97b970044fa1574489b0
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-30 16:21:53 -0600

    added the employees and the user input questions

--- server.js
+++ server.js
@@ -23,5 +23,56 @@ const newDept = async () => {
 
     ]);
     await sql.addDept(department);
+
+    chooseRequest();
 }
 
+//Adding Employees
+const newEmp = async () => {
+    const roleArr = await cHelper.roleChoice();
+    const mgmtArr = await cHelper.mgmtChoices();
+    const emp = await inquirer.createPromptModule([{
+                type: 'input',
+                name: 'first',
+                message: 'What is the Employees first name?',
+                validate: (first) => {
+                    if (first && isNan(first)) {
+                        return true;
+                    } else {
+                        console.log('Please enter a name')
+                        return false;
+                    }
+                },
+
+            },
+            {
+                type: 'input',
+                name: 'last',
+                message: 'What is the Employees last name?',
+                validate: (first) => {
+                    if (first && isNan(last)) {
+                        return true;
+                    } else {
+                        console.log('Please enter a name')
+                        return false;
+                    }
+                },
+            },
+            {
+                type: 'list',
+                name: 'role_id',
+                message: 'What is the Employees Role?',
+                choices: mgmtArr,
+                loop: false,
+            }
+            {
+                type: 'list',
+                name: 'manager_id',
+                message: 'Who is the Employees Manager?',
+                choices: mgmtArr,
+                loop: false,
+            }
+    ]);
+    await sql.addEmp(emp);
+    choiceRequest();
+}
